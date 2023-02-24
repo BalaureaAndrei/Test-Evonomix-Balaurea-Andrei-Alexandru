2ed67dbe20b1e30a46614fd28fd2b1deac57be8b
Andrei <andrei.balaurea@gmail.com>
2023-02-24 17:30:07 +0200

    first commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,10 @@
+console.log("ceva");
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    items: 1,
+  });
+});
